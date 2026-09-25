e049d9c22dd808d7d0d9dfec5f8cb259aad2d6cb
Hamed <181864794+hamed-creator@users.noreply.github.com>
2026-09-25 08:12:16 +0800

    small change skip delta frames

--- app.js
+++ app.js
@@ -371,9 +371,12 @@ export class CameraStream extends EventTarget {
         index += 3;
       }
 
-      if (type === 5 || type === 7) {
-        return true;
-      }
+        if (type === 5 || type === 7) {
+          return true;
+        }
+        if (type === 1) {
+          return false; // Early exit prevents scanning the entire delta payload
+        }
     }
     return false;
   }
